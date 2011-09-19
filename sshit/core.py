6966390e18a362196cc9b6b02085412d9eb958f1
Kenneth Reitz <me@kennethreitz.com>
2011-09-19 14:54:24 -0500

    random password generator

--- sshit/core.py
+++ sshit/core.py
@@ -7,9 +7,14 @@ sshit.core
 This module contains the primary functionality of sshit.
 """
 
-# from collections import namedtuple
+import random
 
-# Dependency = namedtuple('Dependency', ['name', 'type'])
+ALPHABET = 'abcdefghijklmnoprstuvwyxzABCDEFGHIJKLMNOPRSTUVWXYZ'
 
-# dependencies = Dependency
+
+def random_password(length=6):
+    """Returns a random ASCII password of given length."""
+
+    p = ''.join([random.choice(ALPHABET) for i in range(length)])
+    return p
 
