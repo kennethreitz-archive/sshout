faf2e11dca7f7015e121dbb8ab870e5597cfe4dd
Kenneth Reitz <me@kennethreitz.com>
2011-09-19 14:54:30 -0500

    cli interface

--- sshit/cli.py
+++ sshit/cli.py
@@ -1,9 +1,35 @@
 # -*- coding: utf-8 -*-
 
+"""
+sshit.cli
+~~~~~~~~~
+
+This module contains the command-line interface for sshit.
+"""
+
+import sys
+
+from clint import args
+from clint.textui import colored, indent, puts
 
 
-def dependencies_available():
-    pass
+def display_version():
+    print '{0}. {1}'.format(
+        colored.red('sshit'),
+        colored.black('A Kenneth Reitz Project.'))
+
 
 def main():
-    pa
+
+    # Arumengs were passed.
+    if args.get(0) is not None:
+        display_version()
+        sys.exit(1)
+
+    print 'SSH Server started. u: {0}  p: {1}'.format(
+        colored.yellow('kreitz'),
+        colored.yellow('iwefhoihef')
+    )
+
+
+
